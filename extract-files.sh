e22f103011904064bacd8022ed742a526edad855
Dyneteve <dyneteve@pixelexperience.org>
2020-02-23 14:32:13 +0000

    violet: Remove some services
    * Drop OMX VPP compleatly
    * Drop CapabilityConfigStore service
    * Drop MLSHAL compleatly
    * Drop Color Postprocessing service
    * Drop QDUTILS compleatly
    * Drop CVP compleatly
    * Drop WIFI wigig/ WIFI learner compleatly
    * Drop fingerprint extension compleatly

--- extract-files.sh
+++ extract-files.sh
@@ -16,7 +16,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 MK_ROOT="${MY_DIR}"/../../..
 
-HELPER="${MK_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${MK_ROOT}/vendor/hentai/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -59,10 +59,6 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
 
 BLOB_ROOT="${MK_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
-
-sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${BLOB_ROOT}/vendor/bin/hw/vendor.display.color@1.0-service"
-sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${BLOB_ROOT}/vendor/bin/hw/vendor.qti.hardware.cvp@1.0-service"
-sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${BLOB_ROOT}/vendor/lib64/vendor.qti.hardware.cvp@1.0-halimpl.so" "${BLOB_ROOT}/vendor/lib/vendor.qti.hardware.cvp@1.0-halimpl.so"
 
 patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${BLOB_ROOT}/vendor/bin/mlipayd@1.1"
 patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${BLOB_ROOT}/vendor/lib64/libmlipay.so"
