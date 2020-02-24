e6156b6d44bc1b898e9718fa9575b2b622122fa0
Dyneteve <dyneteve@pixelexperience.org>
2020-02-24 17:15:36 +0000

    violet: Use prebuilt kernel

--- device.mk
+++ device.mk
@@ -270,6 +270,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Kernel
+LOCAL_KERNEL := device/xiaomi/violet-kernel/Image.gz-dtb
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.violet
